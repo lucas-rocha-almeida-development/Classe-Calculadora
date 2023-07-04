8e5b5d309539f8db254fc6d452d184e2907ad573
lucalmr <lucalmr12@gmail.com>
2023-07-04 19:15:50 -0300

    Adicionar arquivos de projeto.

---
+++ Classe Calculadora/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_Calculadora {
+    internal class Calculadora {
+
+        //utilizando params parametros variados( indicando que pode receber valores variaveis).
+        public static int Soma(params int[] numeros) {
+            int somas = 0;
+            for(int i =0; i < numeros.Length; i++) {
+                somas += numeros[i];
+            }
+            return somas;
+        }
+    }
+}
---
+++ Classe Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_Calculadora {
+    internal class Program {
+        static void Main(string[] args) {
+            int s1 = Calculadora.Soma( 2, 3, 4, 5 );
+
+            Console.WriteLine("Valores da soma utilizando metodo PARAMS: "+s1);
+
+        }
+    }
+}
